31f11c0721d542d192a06c7a6daef7cc1d0541c9
PixelsAhead <99077557+PixelsAhead@users.noreply.github.com>
2022-02-06 19:37:25 +0530

    Add files via upload

---
+++ PixeLogin - WORKING [ Ver 1.0 ].py
@@ -0,0 +1,23 @@
+#Introducing PixeLogin
+print ("Welcome to Pixe𝕃ogin")
+#My Socials
+print ("by PixelsAhead@YT - https://linktr.ee/PixelsAhead")
+#Starting Actual code
+#--------------------------#
+#Prompting to Enter Username
+print ("Please type your Username here")
+#User Inputs UserName
+username = input();
+#Invalid Username System
+if username != "PixelsAhead" :
+    print ("Uh Oh! That Username Doesnt Exist")
+else :
+    if username == 'PixelsAhead' :
+        #Prompting user to enter password
+        print ("Please type your Password here")
+        password = input ()
+        #Invalid Password System
+        if password != "sub2me" :
+            print ("Uh Oh! Wrong Password")
+        else :
+            print ("Login Succesfully ! , Welcome Back PixelsAhead")
