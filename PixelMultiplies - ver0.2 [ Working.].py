42c30295836cb299b30b42229bb1873332a7867b
PixelsAhead <99077557+PixelsAhead@users.noreply.github.com>
2022-02-08 10:45:40 +0530

    Add files via upload

---
+++ PixelMultiplies - ver0.2 [ Working.].py
@@ -0,0 +1,18 @@
+#Introducing PixelMultiplies
+print ("Welcome to PixelMultiplies")
+#My Socials
+print ("by PixelsAhead@YT - https://linktr.ee/PixelsAhead")
+print ("PixelMultiplies is for getting a list of multiples for a number..")
+#--------------------------#
+#Starting Actual code:
+#Prompting to Enter Multplier
+print ("Please type a number to get its Multiples")
+#User Inputs a Number
+num = int(input("Enter an integer: "))
+for count in range(1, 21):
+    product = num * count
+    print(num, "X", count, "=", product)
+#Some stuff that will help you... i guess
+print ("Thank you for using this code")
+print ("PixelMultiplies by PixelsAhead")
+print ("Visit https://linktr.ee/PixelsAhead for most of my links :D")
