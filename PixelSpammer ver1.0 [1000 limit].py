ebe17ef0defd55e227e5b7c906b9ec96dd0daebf
PixelsAhead <99077557+PixelsAhead@users.noreply.github.com>
2022-02-09 21:35:52 +0530

    Create PixelSpammer ver1.0 [1000 limit].py

---
+++ PixelSpammer ver1.0 [1000 limit].py
@@ -0,0 +1,43 @@
+#Destroy any PC by PixelsAheadYT.py
+#Introducing PixelSpammer
+print ("Welcome to PixelSpammer")
+#My Socials
+print ("by PixelsAhead@YT - https://linktr.ee/PixelsAhead")
+#Stuff no one cares about
+print ("Note : The Spam will occour 1000 times")
+
+#the thing that is needed to start the thing that spams
+spam_confirmation = input("Type /spam.egg : ")
+
+#the thing that spams
+def spam_group():
+    spam_list = [spam , spam , spam2 , spam3 , spam4 , spam5 , spam6 , spam7 ]
+    for SpamStuff in spam_list:
+        print (SpamStuff)
+
+#the thing that stores the stuff for the thing that spams 
+spam = "egg"
+spam2 = "spamming"
+spam3 = "PIXELS AHEAD IS VERY EPIK"
+spam4 = "spam go brrrrrrrr"
+spam5 = "pc go brrrrrrrrrrrr"
+spam6 = "ur brain go brrrrrrrrrrrrr"
+spam7 = "code go brrrrrrrrrrrrrrrrr"
+
+#the thing that stops the thing that spams
+spam_stopper = int(0)
+
+#The Spam Code + Stopper Code
+if spam_confirmation == "/spam.egg":
+    while spam_stopper <= 1000:
+        spam_stopper += 1
+        spam_group()
+else:
+    print ("Spam Status : Stopped , ReStart the Code to Remove This Status")
+
+#the thing that helps me
+if spam_stopper >= 1000:
+    print ("====================================")
+    print ("Thank You For... uh destroying your pc?... **facepalm** ")
+    print ("By PixelsAheadYT\nhttps://LinkTr.ee/PixelsAhead for all my links :D\n")
+    
