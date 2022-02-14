7e89f592136c46a5e1ca365edca40f57ee8b0709
PixelsAhead <99077557+PixelsAhead@users.noreply.github.com>
2022-02-14 15:41:44 +0530

    Create PixelSpammer Ver1.0 [NO LIMIT].py

---
+++ PixelSpammer Ver1.0 [NO LIMIT].py
@@ -0,0 +1,50 @@
+#PixelSpammer Ver1.0 [NO LIMIT].py
+
+#Destroy any PC by PixelsAheadYT.py
+#Introducing PixelSpammer
+print ("Welcome to PixelSpammer")
+#My Socials
+print ("by PixelsAhead@YT - https://linktr.ee/PixelsAhead")
+#Stuff no one cares about
+print ("Note : Type 99999999999999999999999999 for destroying a bad pc")
+
+#the thing that is needed to start the thing that spams
+spam_confirmation = input("Type /spam.egg : ")
+
+if spam_confirmation == "/spam.egg":
+    spam_till = int(input("Enter a Digit , The Spammer Will Spam For Those Many Times : "))
+else:
+    print ("I told Enter a digit , ReStart the Code for a Fresh Start")
+
+#the thing that spams
+def spam_group():
+    spam_list = [spam , spam , spam2 , spam3 , spam4 , spam5 , spam6 , spam7 ]
+    for SpamStuff in spam_list:
+        print (SpamStuff)
+
+#the thing that stores the stuff for the thing that spams 
+spam = "1234"
+spam2 = "123456789"
+spam3 = "123456789123456789"
+spam4 = "123456789123456789123456789123456789"
+spam5 = "123456789123456789123456789123456789123456789123456789"
+spam6 = "123456789123456789123456789123456789123456789123456789123456789123456789"
+spam7 = "Spamming === Spamming === Spamming === SPAMMING === TACOS == PIXELSAHEADYT"
+
+#the thing that stops the thing that spams
+spam_stopper = int(0)
+
+#The Spam Code + Stopper Code
+if spam_confirmation == "/spam.egg":
+    while spam_stopper <= spam_till:
+        spam_stopper += 1
+        spam_group()
+else:
+    print ("Spam Status : Stopped , ReStart the Code to Remove This Status")
+
+#the thing that helps me
+if spam_stopper >= spam_till:
+    print ("====================================")
+    print ("Thank You For... uh destroying your pc?... **facepalm** ")
+    print ("By PixelsAheadYT\nhttps://LinkTr.ee/PixelsAhead for all my links :D\n")
+    
